68fbb4561d17ccc691ab8ce7689ad80e873eb19f
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-08-06 02:32:15 -0300

    Initial commit

---
+++ src/components/History.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { v4 as uuid } from 'uuid';
+import { GameContext } from '../contexts/GameContext';
+
+export default function Board() {
+  const { history, setHistory, setSquares, setIsXNext, setWhoIsWinner } = useContext(GameContext);
+
+  function handleClick(index) {
+    const newHistory = [...history];
+    newHistory.splice(index, Number.MAX_SAFE_INTEGER);
+    setHistory(newHistory);
+
+    setSquares(history[index].squares);
+    setIsXNext(history[index].squares);
+    setWhoIsWinner(history[index].whoIsWinner);
+  }
+
+  return (
+    <div>
+      {history.map((data, index) => (
+        <div key={uuid()} className="history">
+          <button type="button" onClick={() => handleClick(index)}>
+            Voltar para a jogada {index}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
